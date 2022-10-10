d5d5e83e6e9bc3fc23eaa4a3f03485ab2f2f9e77
curp2 <poosklight@gmail.com>
2022-10-10 16:21:39 -0400

    remove scripts folder

--- scripts/update_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dotDirs=("/etc/lightdm" "/etc/X11" "/etc/portage/" "/etc/laptop-mode" "/home/rp/.config/flavours" "/home/rp/.config/snownews" "/home/rp/.config/nethack" "/home/rp/.config/mpv" "/home/rp/.config/neofetch")
-
-for str in ${dotDirs[@]}; do
-	cp -r $str ..
-done
